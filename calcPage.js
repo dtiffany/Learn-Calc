b00d7ce427829a301d8b613fda23f208955d3a15
dtiffany <bobdjt@gmail.com>
2025-05-29 15:56:32 -0700

    Fixed line, but now Willy is on top of blue line

--- calcPage.js
+++ calcPage.js
@@ -223,7 +223,8 @@ function threeSub(Ans1, Ans2, Ans3, isDone) { // 3 Q Submit (Ans1, Ans2, Ans3, T
 
 
             this.newDis = this.distance + "px";
-            document.getElementById('blueWrap').style.width = this.newDis;
+            const blueLine = document.querySelector('#blueWrap .blue-line');
+            blueLine.style.width = this.newDis;
 
             // Move Willy to left edge of the blue line
             const willy = document.getElementById('willyLeft');
@@ -232,7 +233,7 @@ function threeSub(Ans1, Ans2, Ans3, isDone) { // 3 Q Submit (Ans1, Ans2, Ans3, T
 
 
 
-            
+
             //const blueWrap = document.getElementById('blueWrap');
             //blueWrap.style.width = this.newDis;
 
