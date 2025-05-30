4989f4e40d158c8b4af5cb4e25a7a9337d4c1b98
dtiffany <bobdjt@gmail.com>
2025-05-30 09:29:30 -0700

    That made the counter work but broke walking logic

--- calcPage.js
+++ calcPage.js
@@ -197,6 +197,7 @@ function threeSub(Ans1, Ans2, Ans3, isDone) { // 3 Q Submit (Ans1, Ans2, Ans3, T
         newDis: "",
         count: 0,
         disCount: 100,
+        disDisplay,
         
         restartCount: 0,
 
@@ -250,9 +251,9 @@ function threeSub(Ans1, Ans2, Ans3, isDone) { // 3 Q Submit (Ans1, Ans2, Ans3, T
 
 
             
-            this.disCount = this.roundIfNecessary(this.disCount, 11);
+            this.disDisplay = this.roundIfNecessary(this.disCount, 11);
 
-            document.querySelector('#distance-display').textContent = `Distance to Bathroom: ${this.disCount} meters`;
+            document.querySelector('#distance-display').textContent = `Distance to Bathroom: ${this.disDisplay} meters`;
             if (typeof TABIsec === 'function') {
                 TABIsec();
             }
